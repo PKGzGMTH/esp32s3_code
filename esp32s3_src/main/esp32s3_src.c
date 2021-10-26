198851a2dbbf970d2df21a284ca7fa57a6d8b7af
PKGzGMTH <peakungg@gmail.com>
2021-10-26 17:09:35 +0700

    First commit

---
+++ esp32s3_src/main/esp32s3_src.c
@@ -0,0 +1,40 @@
+// H2install library (.h file)  https://vcpkg.io/en/getting-started.html
+// use 921600 byte speed baud rate
+#include <stdio.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "led_strip.h"
+
+#define BLINK_GPIO 48
+#define row 32
+#define col 32
+
+static led_strip_t *pStrip_a;
+uint8_t R,G,B,data[row][col];
+int start = 0;
+
+void app_main(void)
+{
+    char *ourTaskName = pcTaskGetName(NULL);
+    pStrip_a = led_strip_init(0, BLINK_GPIO, 1);
+    pStrip_a->clear(pStrip_a, 50);
+    ESP_LOGI(ourTaskName, "Starting UP...\n");
+    printf("this is prinf() function!\n\n");
+
+    while (1){
+        printf("%c", start);
+        pStrip_a->set_pixel(pStrip_a, 0, 0, 6, 0);
+        pStrip_a->refresh(pStrip_a, 100);
+        for (int i = 0; i < 32; i++){
+            for (int j = 0; j < 32; j++){
+                int value = (random()%99)+1;
+                printf("%c",value);
+            }
+        }
+        pStrip_a->clear(pStrip_a, 50);
+        vTaskDelay(400 / portTICK_PERIOD_MS);
+    }
+}
